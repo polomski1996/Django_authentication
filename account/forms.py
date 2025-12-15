5919beabc3f22d96ae9b76dc31f5e1b2d2cab479
polomski1996 <polomskimaciej5@gmail.com>
2025-12-15 17:44:02 +0100

    allow user to edit their profile

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from .models import Profile
 
 
 
@@ -33,5 +34,10 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
 
 
--- account/views.py
+++ account/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.http import HttpResponseGone
+from .models import Profile
 from .forms import (
     UserRegistrationForm,
-
+    UserEditForm,
+    ProfileEditForm,
 )
 
 @login_required
@@ -25,6 +27,8 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            #CREATE PROFILE FOR USER
+            Profile.objects.create(user=new_user)
             # Create the user profile
             return render(
                 request,
@@ -37,4 +41,31 @@ def register(request):
         request,
         'account/register.html',
         {'user_form': user_form}
+    )
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form,
+        }
     )
